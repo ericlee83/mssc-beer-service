5ce72bb4e6cf531b36e621d607b10fbb18b5daa9
ericlee83 <eric-lee-83@hotmail.com>
2020-06-26 17:05:12 +1000

    add function to controller

---
+++ src/main/java/com/springframework/msscbeerservice/web/controller/BeerController.java
@@ -0,0 +1,36 @@
+package com.springframework.msscbeerservice.web.controller;
+
+import com.springframework.msscbeerservice.web.model.BeerDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RequestMapping("/api/v1/beer")
+@RestController
+public class BeerController {
+
+    @GetMapping({"/{beerId}"})
+    public ResponseEntity<BeerDto> getBeerById(@PathVariable UUID beerId){
+        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto){
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/{beerId}")
+    public void UpdateBeerById(@PathVariable UUID beerId,@RequestBody BeerDto beerDto){
+
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{beerId}")
+    public void deleteBeerById(@PathVariable UUID beerId){
+
+    }
+
+}
---
+++ src/main/java/com/springframework/msscbeerservice/web/model/BeerStyleEnum.java
@@ -0,0 +1,5 @@
+package com.springframework.msscbeerservice.web.model;
+
+public enum BeerStyleEnum {
+    LAGER, ILSNER, STOUT, GOSE,PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
